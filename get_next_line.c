7239f77a890287f2dd0ddfb6639441c5b0b59f7a
rubenpr13 <rubenpinazorobles@gmail.com>
2025-01-14 22:58:08 +0100

    Create a new repository

---
+++ get_next_line.c
@@ -0,0 +1,130 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rpinazo- <rpinazo-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/02 13:32:11 by rpinazo-          #+#    #+#             */
+/*   Updated: 2025/01/03 15:24:27 by rpinazo-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char *line_remaining(char *stat_buff);
+char    *ft_line(char *stat_buff);
+char    *read_buff(int fd, char *stat_buff);
+
+
+char    *get_next_line(int fd)
+{
+    static char *stat_buff;
+    char        *line;
+
+    stat_buff = read_buff(fd, stat_buff);
+    if (!stat_buff)
+        return (NULL);
+        
+    if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
+    {
+        free(stat_buff);
+        stat_buff = NULL;
+        return (NULL);
+    }
+    if (ft_strlen(stat_buff) < 1)
+    {
+        free(stat_buff);
+        stat_buff = NULL;
+        return(NULL);
+    }
+    line = ft_line(stat_buff);
+    stat_buff = line_remaining(stat_buff);
+    return (line);
+}
+
+char *line_remaining(char *stat_buff)
+{
+    char    *line;
+    int     i;
+    int     j;
+
+    i = 0;
+    while (stat_buff[i] && stat_buff[i] != '\n')
+        i++;
+    if (!stat_buff[i])
+    {
+        free(stat_buff);
+        stat_buff = NULL;
+        return (NULL);
+    }
+    line = ft_calloc((ft_strlen(stat_buff) - i + 1), sizeof(char));
+    i++;
+    j = 0;
+    while(stat_buff[i])
+        line[j++] = stat_buff[i++];
+    free(stat_buff);
+    return(line);
+}
+
+char    *ft_line(char *stat_buff)
+{
+    char    *line;
+    int     i;
+
+    i = 0;
+    if(!stat_buff[i])
+        return(NULL);
+    while (stat_buff[i] && stat_buff[i] != '\n')
+        i++;
+    line = ft_calloc(i + 2, sizeof(char));
+    i = 0;
+    while (stat_buff[i] && stat_buff[i] != '\n')
+    {
+        line[i] = stat_buff[i];
+        i++;
+    }
+    if (stat_buff[i] && stat_buff[i] == '\n')
+    {
+        line[i] = stat_buff[i];
+        i++;
+    }
+    line[i] = '\0';
+    return (line);
+}
+
+char    *read_buff(int fd, char *stat_buff)
+{
+    char    *buffer;
+    int     result;
+
+    if (!stat_buff)
+        stat_buff = ft_calloc(1, sizeof(char));
+    buffer = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
+    if (!buffer)
+    {
+        free(stat_buff);
+        stat_buff = NULL;
+        return (NULL);
+    }
+    
+    result = 1;
+    while (result > 0)
+    {
+        result = read(fd, buffer, BUFFER_SIZE);
+        if (result < 0)
+        {
+            free(buffer);
+            buffer = NULL;
+            free(stat_buff);
+            stat_buff = NULL;
+            return (NULL);
+        }
+        buffer[result] = '\0';
+        stat_buff = ft_strjoin(stat_buff, buffer);
+        if (ft_strchr(buffer, '\n') == 1)
+            break;
+    }
+    free(buffer);
+    return (stat_buff);
+}
---
+++ get_next_line_utils.c
@@ -0,0 +1,83 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rpinazo- <rpinazo-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/12 23:02:28 by rpinazo-          #+#    #+#             */
+/*   Updated: 2024/10/16 11:24:49 by rpinazo-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+int ft_strlen(char *str)
+{
+    int i;
+
+    if (!str)
+        return (0);
+    i = 0;
+    while (str[i] != '\0')
+        i++;
+    return (i);
+}
+
+char    *ft_strjoin(char *s1, char *s2)
+{
+    int     len;
+    char    *str;
+    int     i;
+    int     j;
+    
+    len = (ft_strlen(s1) + ft_strlen(s2));
+	str = malloc((len + 1) * sizeof(char));
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2[j] != '\0')
+	{
+		str[i + j] = s2[j];
+		j++;
+	}
+	str[(i + j)] = '\0';
+	free(s1);
+	return (str);
+}
+
+void	*ft_calloc(size_t size, size_t type)
+{
+	char	*str;
+	size_t	i;
+
+	str = (malloc(size * type));
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (i < (size * type))
+		str[i++] = 0;
+	return (str);
+}
+
+int	ft_strchr(char *str, char chr)
+{
+	int	i;
+
+	i = 0;
+	if (chr == '\0')
+		return (0);
+	while (str[i] != '\0')
+	{
+		if (str[i] == chr)
+			return (1);
+		i++;
+	}
+	return (0);
+}
