5367603b2b7b2dbb433c1e176ea47fc13b027caa
rubenpr13 <rubenpinazorobles@gmail.com>
2025-03-07 11:24:40 +0100

    Include tester

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: rpinazo- <rpinazo-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/02 13:32:11 by rpinazo-          #+#    #+#             */
-/*   Updated: 2025/03/06 15:42:21 by rpinazo-         ###   ########.fr       */
+/*   Updated: 2025/03/07 11:11:16 by rpinazo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -130,21 +130,21 @@ char    *read_buff(int fd, char *stat_buff)
     return (stat_buff);
 }
 
-int main(int argc, char **argv)
-{
-    int fd;
-    char *line;
+// int main(int argc, char **argv)
+// {
+//     int fd;
+//     char *line;
 
-    if (argc == 2)
-    {
-        fd = open(argv[1], O_RDONLY);
-        while ((line = get_next_line(fd)))
-        {
-            printf("%s", line);
-            free(line);
-        }
-        free(line);
-        close(fd);
-    }
-    return (0);
-}
+//     if (argc == 2)
+//     {
+//         fd = open(argv[1], O_RDONLY);
+//         while ((line = get_next_line(fd)))
+//         {
+//             printf("%s", line);
+//             free(line);
+//         }
+//         free(line);
+//         close(fd);
+//     }
+//     return (0);
+// }
